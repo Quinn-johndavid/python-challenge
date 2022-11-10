7ee97633bb04ce2382e65e7ec455db6004a0f128
quinn-johndavid <quinn.johndavid@gmail.com>
2022-11-10 14:00:11 -0600

    added comments to code, final formating code

--- Pybank/main.py
+++ Pybank/main.py
@@ -3,16 +3,17 @@ import csv
 
 # Path to collect data from the Resources folder
 budgetDataRead = "Resources/budget_data.csv"
+#Path to output analysis
 budgetDataAnalysis = "Analysis/budgetData.txt"
 
 totalMonths = []
 totalProfit = []
 monthlyPChange = []
-
+#opens the csv and seperates the collumns based on the headers ,'s then skips the header for rowcounting purposes, without there would be 4 candidates in the list one of them being "candidate" with 0 votes
 with open (budgetDataRead) as BDR:
     BDR_reader = csv.reader(BDR,delimiter=",")
     bdrHeader = next(BDR_reader)
-
+#adds the values from the first column in the csv and adds it to its respective array
     for row in BDR_reader:
 
         totalMonths.append(row[0])
@@ -22,14 +23,16 @@ with open (budgetDataRead) as BDR:
     for i in range(len(totalProfit)-1):
 
         monthlyPChange.append(totalProfit[i+1]-totalProfit[i])
-
+#Finds the min and max of the monthlyPchange array and then 
+#Correlate max and min to the proper month using month list and index associated 
+#+ 1 at the end since month associated with change is the next month
 maxProfit = max(monthlyPChange)
 minProfit = min(monthlyPChange)
 monthlyMaxProfit = monthlyPChange.index(max(monthlyPChange)) + 1 
 monthlyMinProfit = monthlyPChange.index(min(monthlyPChange)) + 1
 
 
- 
+ #Sloppy, left out in the open, output variables are the way to go
 """ print("Financial Analysis")
 print("---------------------------")
 print(f"Total Months: {len(totalMonths)}")
@@ -38,6 +41,8 @@ print(f"Average Revenue Change: ${round(sum(monthlyPChange)/len(monthlyPChange),
 print(f"Greatest Increase in Profits: {totalMonths[monthlyMaxProfit]} ${(str(maxProfit))}")
 print(f"Greatest Decrease in Profits: {totalMonths[monthlyMinProfit]} ${(str(minProfit))}")
  """
+
+#Output Variables are amazing
 output = (
     f"Financial Analysis\n"
     f"---------------------------\n"
@@ -50,6 +55,6 @@ output = (
 )
 
 print(output)
-
+#Writes final analysis to Analysis file
 with open(budgetDataAnalysis, "w") as endfile:
     endfile.write(output)
--- pyPoll/main.py
+++ pyPoll/main.py
@@ -1,9 +1,9 @@
 import os 
 import csv 
-
+#input and output file paths
 pollDataRead = "Resources/election_data.csv"
 pollAnalysisOut = "Analysis/electionAnalysis.txt"
-
+#array, dictionary and variable
 totalVotes = 0
 candidateNameStore = []
 candidateVotes = {}
@@ -11,6 +11,7 @@ candidateVotes = {}
 pollWinnerCandName = ""
 pollWinCount = 0
 
+#opens the filepath and then seperates the columns by comma's in the csv
 with open(pollDataRead) as PDR:
     PDRReader = csv.reader(PDR,delimiter=",")
 
@@ -20,12 +21,12 @@ with open(pollDataRead) as PDR:
     for row in PDRReader:
 
         
-
+#adds the votes to their respective candidate, and is directed to column2 
         totalVotes = totalVotes + 1
 
         candidateName = row[2]
 
-
+#adds candidate if candidate is not in the name array, then sets their total vote count to 0 to initilize the variable and then for each time their name 
         if candidateName not in candidateNameStore:
 
             candidateNameStore.append(candidateName)
@@ -33,6 +34,7 @@ with open(pollDataRead) as PDR:
             candidateVotes[candidateName] = 0 
         candidateVotes[candidateName] = candidateVotes[candidateName] + 1
 
+#Start of the Analysis
 with open(pollAnalysisOut, "w") as analysisOut:
 
     pollResults = (
@@ -43,11 +45,11 @@ with open(pollAnalysisOut, "w") as analysisOut:
         f"------------------------\n"
     )
     print(pollResults)
-
+    #quick summary of the poll results and outputs to terminal and then the analysis file 
     analysisOut.write(pollResults)
 
     for candidate in candidateVotes:
-
+#grabs the amount of votes for each candidate, then calculates the perecentage of vote
         votes = candidateVotes.get(candidate)
         percentOfVote = float(votes)/float(totalVotes) *100
 
@@ -55,17 +57,18 @@ with open(pollAnalysisOut, "w") as analysisOut:
             pollWinCount = votes
             pollWinnerCandName = candidate
 
-
+#strings together the candidate name, their percentage of the vote, and the actual amount of their votes, then prints to terminal and adds to the analysis file 
         pollOutput = f"{candidate}: {percentOfVote:.2f}% ({votes})\n"
         print(pollOutput)
 
         analysisOut.write(pollOutput)
     
+    #Output variable, super cool
     summaryOfPollResults = (
         f"------------------------------\n"
         f"Winner: {pollWinnerCandName}\n"
         f"-----------------------------\n"
     )
     print(summaryOfPollResults)
-
+    #writes the aforementioned output variable to the output file 
     analysisOut.write(summaryOfPollResults)
